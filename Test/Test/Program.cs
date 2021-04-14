9ff6dcb9a14fead478022d53173641e0b74378bf
fogartyy <kmaster1098765@gmail.com>
2021-04-14 17:19:33 +1200

    Control

---
+++ Test/Test/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using SharpDX.DirectInput;
+using Ticks;
+using Handling;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            
+
+            var autoEvent = new AutoResetEvent(false);
+
+            var tick = new Tick();
+
+            var stateTimer = new Timer(tick.CheckStatus, 
+                                   autoEvent, 0, 1);
+
+            var handling = new DataHandling(tick);
+            
+            var handlingTimer = new Timer(handling.MakeCommand,
+                                    autoEvent, 0, 1);
+
+            // Initialize DirectInput
+            var directInput = new DirectInput();
+
+            // Find a Joystick Guid
+            var joystickGuid = Guid.Empty;
+
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad,
+                        DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Gamepad not found, look for a Joystick
+            if (joystickGuid == Guid.Empty)
+                foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick,
+                        DeviceEnumerationFlags.AllDevices))
+                    joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Joystick not found, throws an error
+            if (joystickGuid == Guid.Empty)
+            {
+                Console.WriteLine("No joystick/Gamepad found.");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            // Instantiate the joystick
+            var joystick = new Joystick(directInput, joystickGuid);
+
+            Console.WriteLine("Found Joystick/Gamepad with GUID: {0}", joystickGuid);
+
+            // Query all suported ForceFeedback effects
+            var allEffects = joystick.GetEffects();
+            foreach (var effectInfo in allEffects)
+                Console.WriteLine("Effect available {0}", effectInfo.Name);
+
+            // Set BufferSize in order to use buffered data.
+            joystick.Properties.BufferSize = 128;
+
+            // Acquire the joystick
+            joystick.Acquire();
+
+            // Poll events from joystick
+
+            while (true)
+            {
+                joystick.Poll();
+                var datas = joystick.GetBufferedData();
+                foreach (var state in datas)
+                {
+                    if (state.Offset == JoystickOffset.X)
+                    {
+                        tick.servo1Change = (int)Math.Round((state.Value - 32768) * 0.0002);
+                    }
+
+                    if (state.Offset == JoystickOffset.Y)
+                    {
+                        tick.servo2Change = (int)Math.Round((state.Value - 32768) * 0.0002);
+                    }
+
+                    if (state.Offset == JoystickOffset.Z)
+                    {
+                        tick.speed = (float)(((state.Value * -0.0008) + 53)/15);
+ 
+                    }
+                    if (state.Offset == JoystickOffset.Buttons1)
+                    {
+                        Console.WriteLine($"Switch Mode :{(state.Value)}");
+                    }
+                    if (state.Offset == JoystickOffset.Buttons6)
+                    {
+                        Console.WriteLine($"TriggerOpen :{(state.Value)}");
+                    }
+                    if (state.Offset == JoystickOffset.Buttons0)
+                    {
+                        Console.WriteLine($"TriggerClose :{(state.Value)}");
+                    }
+
+                }
+            }
+
+
+
+
+  
+        }
+    }
+
+}
---
+++ Test/Test/Tick.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Ticks
+{
+    class Tick
+    {
+        int max = 180;
+        int min = 0;
+        public float speed = 1;
+        public int servo1 = 90;
+        public int servo1Change = 0;
+        public int servo2 = 90;
+        public int servo2Change = 0;
+        public int servo3 = 90;
+        public int servo3Change = 0;
+        public int servo4 = 90;
+        public int servo4Change = 0;
+
+        public Tick()
+        {
+            
+        }
+
+        public void CheckStatus(Object stateInfo)
+        {
+            AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+            
+            if (servo1 + servo1Change*speed <= max && servo1 + servo1Change*speed >= min & servo1Change*speed != 0)
+            {
+                servo1 = servo1 + servo1Change;
+                Console.WriteLine($"X :{servo1}");
+            }
+
+            if (servo2 + servo2Change*speed <= max && servo2 + servo2Change*speed >= min & servo2Change*speed != 0)
+            {
+                servo2 = servo2 + servo2Change;
+                Console.WriteLine($"Y :{servo2}");
+            }
+
+
+        }
+    }
+}
